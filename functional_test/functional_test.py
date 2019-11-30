a3a8a414e5f1583178c0a3f7d8a0dcb79ba72da2
clov <clovis.mugaruka@gmail.com>
2019-11-30 13:58:39 +0300

    fixed PATH issue

--- functional_test/functional_test.py
+++ functional_test/functional_test.py
@@ -1,13 +1,16 @@
+import os
 import pytest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
+ROOT = os.path.dirname(os.path.dirname(__file__))
+
 @pytest.fixture
 def browser():
     """creates an instance of Chrome driver."""
     # initialize browser
-    driver = webdriver.Chrome('../utils/chromedriver')
+    driver = webdriver.Chrome(os.path.join(ROOT, 'utils/chromedriver')) # TODO: this can be removed if driver is already installed.
     driver.implicitly_wait(10)
     driver.get('https://demo.applitools.com/hackathon.html')
     yield driver
@@ -20,7 +23,6 @@ class TestUIElement:
 
 class TestDataDriven:
     """For all Data Driven tests."""
-
     def test_login_without_providing_credentials(self, browser):
         browser.find_element_by_id('log-in').click()
         assert 'Both Username and Password must be present' in \
@@ -33,7 +35,7 @@ class TestDataDriven:
                browser.find_element_by_class_name('alert-warning').text
 
     def test_login_only_provide_password(self, browser):
-        browser.find_element_by_id('username').send_keys('ada')
+        browser.find_element_by_id('password').send_keys('lovelace')
         browser.find_element_by_id('log-in').click()
         assert 'Username must be present' in \
                browser.find_element_by_class_name('alert-warning').text
