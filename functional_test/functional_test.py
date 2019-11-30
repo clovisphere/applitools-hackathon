06f40f5cc1d1208a3e9570c7003ba13852fd3e9f
clov <clovis.mugaruka@gmail.com>
2019-11-30 12:28:13 +0300

    added functional test for logging in without credential

--- functional_test/functional_test.py
+++ functional_test/functional_test.py
@@ -1,14 +1,24 @@
 import pytest
-from selenium.webdriver import Chrome
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys 
 
 @pytest.fixture
 def browser():
     """creates an instance of Chrome driver."""
-    driver = Chrome()
+    # used to maximize window
+    option = webdriver.ChromeOptions()
+    option.add_argument("--start-maximized")
+    # initialize browser
+    driver = webdriver.Chrome(options=option)
     driver.implicitly_wait(10)
+    driver.get('https://demo.applitools.com/hackathon.html')
     yield driver
     driver.quit()
 
 class TestUIElement:
     pass
+
+class TestDataDriven:
+    def test_login_without_providing_credentials(self, browser):
+        browser.find_element_by_id('log-in').click()
+        assert 'Both Username and Password must be present' in browser.find_element_by_class_name('alert-warning').text
