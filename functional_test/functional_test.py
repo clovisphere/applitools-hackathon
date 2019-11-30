6a5ef7aee7ab910fbbd7d1b2aa70a31d20261eff
clov <clovis.mugaruka@gmail.com>
2019-11-30 13:42:23 +0300

    updated

--- functional_test/functional_test.py
+++ functional_test/functional_test.py
@@ -1,24 +1,39 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.keys import Keys
+
 
 @pytest.fixture
 def browser():
     """creates an instance of Chrome driver."""
-    # used to maximize window
-    option = webdriver.ChromeOptions()
-    option.add_argument("--start-maximized")
     # initialize browser
-    driver = webdriver.Chrome(options=option)
+    driver = webdriver.Chrome('../utils/chromedriver')
     driver.implicitly_wait(10)
     driver.get('https://demo.applitools.com/hackathon.html')
     yield driver
     driver.quit()
 
+
 class TestUIElement:
     pass
 
+
 class TestDataDriven:
+    """For all Data Driven tests."""
+
     def test_login_without_providing_credentials(self, browser):
         browser.find_element_by_id('log-in').click()
-        assert 'Both Username and Password must be present' in browser.find_element_by_class_name('alert-warning').text
+        assert 'Both Username and Password must be present' in \
+               browser.find_element_by_class_name('alert-warning').text
+
+    def test_login_only_provide_username(self, browser):
+        browser.find_element_by_id('username').send_keys('ada')
+        browser.find_element_by_id('log-in').click()
+        assert 'Password must be present' in \
+               browser.find_element_by_class_name('alert-warning').text
+
+    def test_login_only_provide_password(self, browser):
+        browser.find_element_by_id('username').send_keys('ada')
+        browser.find_element_by_id('log-in').click()
+        assert 'Username must be present' in \
+               browser.find_element_by_class_name('alert-warning').text
